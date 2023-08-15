dc48542139f27e240eb9cbed94e6cd4efeb0e005
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-15 09:33:56 +0200

    2nd test passed

--- src/openOrSenior.test.js
+++ src/openOrSenior.test.js
@@ -4,4 +4,8 @@ describe('openOrSenior', () => {
   it("[[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]] should return ['Open', 'Senior', 'Open', 'Senior']", () => {
     expect(openOrSenior([[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]])).toStrictEqual(['Open', 'Senior', 'Open', 'Senior']);
   });
+
+  it("[[45, 12],[55, 21],[19, -2],[104, 20]] should return ['Open', 'Senior', 'Open', 'Senior']", () => {
+    expect(openOrSenior([[45, 12],[55, 21],[19, -2],[104, 20]])).toStrictEqual(['Open', 'Senior', 'Open', 'Senior']);
+  });
 });
